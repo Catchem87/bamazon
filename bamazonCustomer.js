9fc0150515e11255c0f54d2e7d366aa57402228f
Catchem87 <moras.ashley87@gmail.com>
2019-01-07 18:10:38 -0500

    problem resolved, missing space before WHERE

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,44 +10,50 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
-    connection.query("SELECT * FROM products", function(err, result, fields){
+    connection.query("SELECT * FROM products", function (err, result, fields) {
         if (err) throw err;
-        console.log(result);
+        for (var i = 0; i < result.length; i++) {
+            console.log("Item ID: " + result[i].item_id + "\n" + "Product Name: " + result[i].product_name + "\n" + "Department: " + result[i].department_name + "\n" + "Price Per Unit: " + result[i].price + "\n" + "Available Units: " + result[i].stock_quantity + "\n");
+        };
+        // console.log(result);
         requestOrder();
     });
-  });
+});
 
-  function requestOrder() {
-      inquirer
+function requestOrder() {
+    inquirer
         .prompt([{
-            name: "id",
-            type: "input",
-            message: "Enter the item ID of the item you would like to purchase."
-        },
-        {
-            name: "quantity",
-            type: "input",
-            message: "Enter the quantity that you would like to purchase."
-        }
-    ]).then(function(answer) {
-        var query = "SELECT * FROM products WHERE ?";
-        connection.query(query, { item_id: answer.id }, function(err, res) {
-            if(err) throw err;
-            if(res[0].stock_quantity < answer.quantity){
-                console.log("Insufficient quantity in stock to fulfill order.");
-            } else {
-                var updatedQuantity = res[0].stock_quantity - answer.quantity;
-                var totalPrice = answer.quantity * res[0].price;
-                var id = answer.id;
-                console.log("Total Price: $" + totalPrice.toFixed(2));
-                var sql = "UPDATE products SET stock_quantity = " + updatedQuantity + "WHERE item_id = " + id;
-                connection.query(sql, function(err, result) {
-                    if (err) throw err;
-                    console.log(result.affectedRows + " record(s) updated");
-                });
-            };
+                name: "id",
+                type: "input",
+                message: "Enter the item ID of the item you would like to purchase."
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "Enter the quantity that you would like to purchase."
+            }
+        ]).then(function (answer) {
+            var query = "SELECT * FROM products WHERE ?";
+            connection.query(query, {
+                item_id: answer.id
+            }, function (err, res) {
+                if (err) throw err;
+                if (res[0].stock_quantity < answer.quantity) {
+                    console.log("Insufficient quantity in stock to fulfill order.\n");
+                    requestOrder();
+                } else {
+                    var updatedQuantity = res[0].stock_quantity - answer.quantity;
+                    var totalPrice = answer.quantity * res[0].price;
+                    var id = answer.id;
+                    var sql = "UPDATE products SET stock_quantity = " + updatedQuantity + " WHERE item_id = " + id;
+                    connection.query(sql, function (err, result) {
+                        if (err) throw err;
+                        console.log("Total Price: $" + totalPrice.toFixed(2));
+                        connection.end();
+                    });
+                };
+            });
         });
-    });
 }
